6b4cc07b1b1b018c07cee1870039134043fea989
Carl Tompkins <carl@ctomp.io>
2019-04-16 19:12:57 -0400

    Update player/character data

--- src/Characters.js
+++ src/Characters.js
@@ -78,6 +78,75 @@ const characters = {
     name: "The Hound",
     imageUrl:
       "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/episodes/4/37/episode-37-03-1920.jpg/_jcr_content/renditions/cq5dam.web.980.551.jpeg"
+  }),
+  Samwell: new CharacterInfo({
+    name: "Samwell Tarley",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/samwell-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  Gilly: new CharacterInfo({
+    name: "Gilly",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s6/gilly-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  Sam: new CharacterInfo({
+    name: "Baby Sam",
+    imageUrl: ""
+  }),
+  Varys: new CharacterInfo({
+    name: "Lord Varys",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/maetser-varys-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  Brienne: new CharacterInfo({
+    name: "Brienne of Tarth",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/brienne-of-tarth-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  Davos: new CharacterInfo({
+    name: "Davos Seaworth",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/davos-seaworth-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  Bronn: new CharacterInfo({
+    name: "Bronn",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/bronn-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  Podrick: new CharacterInfo({
+    name: "Podrick Payne",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/podrick-payne-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  Tormund: new CharacterInfo({
+    name: "Tormund Giantsbane",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/tormund-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  "Grey-Worm": new CharacterInfo({
+    name: "Grey Worm",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s3/grey-worm-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  Missandei: new CharacterInfo({
+    name: "Missandei",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/missandei-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  Gendry: new CharacterInfo({
+    name: "Gendry",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/gendry-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  Beric: new CharacterInfo({
+    name: "Beric Dondarrion",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/beric-dondarron-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
+  }),
+  Euron: new CharacterInfo({
+    name: "Euron Greyjoy",
+    imageUrl:
+      "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s6/euron-greyjoy-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg"
   })
 };
 
